cb79c34df1240b6b72d719b5060687c34ae380bf
Miles Smith <miles-smith@omrf.org>
2019-02-20 13:09:42 -0600

    fixed grouping problems and a whole lot more

--- R/groupedViolins.R
+++ R/groupedViolins.R
@@ -1,16 +1,16 @@
 #' @title groupedViolins
-#' 
-#' @description Produce grouped violin plots.  Suitable for 
+#'
+#' @description Produce grouped violin plots.  Suitable for
 #' plotting cluster or cell type specific marker genes.
 #'
 #' @param object Seurat object to plot
 #' @param genes List of genes to plot
 #' @param marker_list A table of cluster markers as produced by FindMarkers()
-#'  or FindAllMarkers or a two column data.frame with the columns "cluster" and 
-#'  "gene".  If provided, supercedes the genes argument.  Note: if a marker is 
-#'  present in multiple clusters, only the first (as determined by arrange()) is used. 
+#'  or FindAllMarkers or a two column data.frame with the columns "cluster" and
+#'  "gene".  If provided, supercedes the genes argument.  Note: if a marker is
+#'  present in multiple clusters, only the first (as determined by arrange()) is used.
 #'  Default: NULL
-#' @param group_by Grouping variable from ident or meta data to use.
+#' @param group_var Grouping variable from ident or meta data to use.
 #' @param plot_title What it says on the tin.  Default: NULL
 #' @param assay_use Assay to plot.  Default: "RNA"
 #' @param flip Should genes be displayed along the y-axis? Default: TRUE
@@ -18,61 +18,67 @@
 #' @importFrom Seurat FetchData
 #' @importFrom tidyr gather
 #' @importFrom tibble rownames_to_column
-#' @importFrom plyr mapvalues
+#' @importFrom dplyr group_by arrange select slice
+#' @importFrom stringr str_detect
+#' @importFrom magrittr %>% %<>%
 #' @import ggplot2
 #'
 #' @return
 #' @export
 #'
 #' @examples
-groupedViolins <- function(object, 
-                         genes = NULL, 
+groupedViolins <- function(object,
+                         genes = NULL,
                          marker_list = NULL,
-                         group_by,
+                         group_var = NULL,
                          plot_title = NULL,
                          assay_use = "RNA",
                          flip = TRUE){
-  
-  # if there is a simpler/better way to handle arguments 
+
+  # if there is a simpler/better way to handle arguments
   # being either quoted or unquoted, I'd love to hear about it.
   if (is.null(genes) & is.null(marker_list)){
     stop("No genes or marker_list provided.  What am I supposed to plot?")
   }
   try(
-    if (is.character(group_by)) {
-      group_by <- as.name(substitute(group_by))
+    if (is.null(group_var)){
+      group_var <- "ident"
+    }, silent = TRUE)
+  try(
+    if (is.character(group_var)) {
+      group_var <- as.name(substitute(group_var))
     }, silent = TRUE
   )
 
   if (!is.null(marker_list)){
     marker_list %<>% select(cluster, gene) %>%
+      mutate_if(is.factor, as.character) %>%
       arrange(gene) %>%
       group_by(gene) %>%
       slice(1)
-    genes <- marker_list %>% pull(gene)
+    genes <- marker_list %>% pull(gene) %>% as.character()
   }
-  
-  group_by <- enquo(group_by)
-  type_expr <- FetchData(object, 
-                         vars = c(unique(genes), quo_name(group_by)))
-  type_expr %<>% 
-    rownames_to_column('name') %>% 
+
+  group_var <- enquo(group_var)
+  type_expr <- FetchData(object,
+                         vars = c(unique(genes), quo_name(group_var)))
+  type_expr %<>%
+    rownames_to_column('name') %>%
     gather(key = "gene",
            value = "value",
            -name,
-           -!!group_by)
+           -!!group_var)
 
   if (!is.null(marker_list)){
-    type_expr$cluster_marker <- mapvalues(x = type_expr[["gene"]],
-                                          from = marker_list[["gene"]],
-                                          to = marker_list[["cluster"]])
+    type_expr %<>% inner_join(marker_list, by = "gene")
     # fix the ordering of the clusters in the event they are all numbers
     # so that 0 < 1 < 2 ... < n + 1 instead or 0 < 1 < 10 < 2
-    if(all(grep(pattern = "[::digit::]+", x = type_expr[["cluster_marker"]]))){
-    type_expr[["cluster_marker"]] %<>% as.integer() %>% as.factor()
+    if(all(str_detect(pattern = "[0-9]+",
+                      string = type_expr[["cluster"]]))){
+      type_expr[["cluster"]] %<>% as.integer() %>% as.factor()
     }
   }
-  
+
   type_violins <- type_expr %>%
     ggplot() +
     theme(legend.position = "none",
@@ -87,12 +93,6 @@ groupedViolins <- function(object,
           strip.background = element_blank(),
           strip.text.x = element_text(face = "italic")) +
     labs(x = "", y = "")
-  
-  # if (!is.null(marker_list)){
-  #   type_violins <- type_violins + 
-  # } else {
-  #   type_violins <- type_violins +
-  # }
 
   if (isTRUE(flip)){
     type_violins <- type_violins + coord_flip()
@@ -100,17 +100,17 @@ groupedViolins <- function(object,
 
   if (!is.null(marker_list)){
     type_violins <- type_violins +
-      geom_violin(aes(x = !!group_by,
-                      y = value, 
-                      fill = cluster_marker),
+      geom_violin(aes(x = !!group_var,
+                      y = value,
+                      fill = cluster),
                   scale = "width",
                   trim = TRUE) +
-      facet_grid(~cluster_marker+gene,
+      facet_grid(~cluster+gene,
                  scales = "free")
   } else {
     type_violins <- type_violins +
-      geom_violin(aes(x = !!group_by,
-                      y = value, 
+      geom_violin(aes(x = !!group_var,
+                      y = value,
                       fill = gene),
                   scale = "width",
                   trim = TRUE) +
@@ -121,15 +121,4 @@ groupedViolins <- function(object,
     type_violins <- type_violins + labs(title = plot_title)
   }
   type_violins
-}
-
-
-marker_by_cluster_violins <- function(object, 
-                                      markers,
-                                      ...){
-  lapply(unique(markers$cluster), function(i){
-    j <- markers %>% filter(cluster == i) %>% pull(gene)
-    lineage_violins(object = object, genes = j, ...) +
-      labs(title = glue::glue("Cluster {i}"))
-  })
 }
